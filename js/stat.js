621ea93c3364a944698014492d731b05875fb7eb
Artem Ustas <art4yus@ya.ru>
2017-01-26 02:19:08 +0300

    Исправил пару ошибок от ESLint (на потерянный знак ; ) + дописал тень для прямоугольника

--- js/stat.js
+++ js/stat.js
@@ -1,12 +1,15 @@
-'use strict'
+'use strict';
 
 var window = {
-  renderStatistics: function(ctx, names, times) {
+  renderStatistics: function (ctx, names, times) {
     var canvas = document.querySelector('canvas');
-    var ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(110, 20, 420, 270);
 
     ctx.fillStyle = 'white';
     ctx.fillRect(100, 10, 420, 270);
 
-  };
+  }
 };

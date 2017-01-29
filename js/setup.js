3af2ec4dfe060e1d7f3b36e95ab445fbf7bcc104
Artem Ustas <art4yus@ya.ru>
2017-01-29 23:22:54 +0300

    Добавил действие (показать настройки персонажа) при нажатии на иконку игрока

--- js/setup.js
+++ js/setup.js
@@ -1,1 +1,8 @@
 'use strict';
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+
+setupOpen.addEventListener('click', function () {
+  setup.classList.remove('invisible');
+});

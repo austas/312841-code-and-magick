c330211ef666ebfdf19e04bc5068e2fc3e749afa
Artem Ustas <art4yus@ya.ru>
2017-01-29 19:28:27 +0300

    Объявил функции для отрисовки прямоугольников и текста. Для текста дописал функцию проверки цвета (на вход) на случай, если захотим менять цвет вывода. По умолчанию - черный, но теперь может быть любой.

--- js/stat.js
+++ js/stat.js
@@ -2,44 +2,52 @@
 
 window.renderStatistics = function (ctx, names, times) {
 
+// Функция генерирует случайное число. Будем использовать ее для генерации случайного цвета
   function getRandom() {
     return parseInt(((Math.random() * 5) * 50), 10);
   }
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+// Функция для рисования прямоугольников по заданным параметрам
+  function drawRect(x1, y1, widthRect, heightRect, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x1, y1, widthRect, heightRect);
+  }
 
-  ctx.fillStyle = 'white';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+// Функция для рисования текста
+  function drawText(message, x1, y1, color) {
+    ctx.fillStyle = color || 'black';
+    ctx.textBaseline = 'hanging';
+    ctx.font = '16px PT Mono';
+    ctx.fillText(message, x1, y1);
+  }
 
-  ctx.fillStyle = 'black';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 120, 30);
-  ctx.fillText('Список результатов:', 120, 50);
+// Рисуем подложку + тень + текст
+  drawRect(110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+  drawRect(99, 9, 422, 272); // этот прямоугольник для того, чтобы создать границу, используя уже заданную функцию
+  drawRect(100, 10, 420, 270, 'white');
 
+  drawText('Ура вы победили!', 120, 30);
+  drawText('Список результатов:', 120, 50);
+
+// Ищем максимальное время среди игроков, далее будем использовать его для подсчета макс. высоты гистограммы
   var max = -1;
-
   for (var i = 0; i < times.length; i++) {
     if (times[i] > max) {
       max = times[i];
     }
   }
 
+// Отрисовываем гистограммы в соот. с временем прохождения игроками игры + выводим эти значения
   for (i = 0; i < names.length; i++) {
     var stepX = 155 + 90 * i;
     var heightHist = (150 / max) * times[i];
-    var stepHist = 245 - heightHist;
 
-    var randomColor = ['rgb(' + getRandom() + ', ' + getRandom() + ', ' + getRandom() + ')'];
-    var colorHist = (names[i] === 'Вы') ? 'red' : randomColor;
+    var randomColor = ['rgb(0, ' + getRandom() + ', ' + getRandom() + ')'];
+    var color = (names[i] === 'Вы') ? 'red' : randomColor;
 
-    ctx.fillStyle = colorHist;
-    ctx.fillRect(stepX, stepHist, 40, heightHist);
+    drawRect(stepX, 245 - heightHist, 40, heightHist, color);
+    drawText(names[i], stepX, 255);
+    drawText(parseInt(times[i], 10), stepX, 225 - heightHist);
 
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[i], stepX, 255);
-    ctx.fillText(parseInt(times[i], 10), stepX, 225 - heightHist);
   }
 };

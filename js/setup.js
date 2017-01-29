94a9f3c05d3c3db5d63a283b4769548b3c4df259
Artem Ustas <art4yus@ya.ru>
2017-01-29 23:28:43 +0300

    Добавил действие при нажатии на крестик (в профиле игрока) - закрытие этого окна

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,13 @@
 'use strict';
 
 var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
 var setup = document.querySelector('.setup');
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
+});
+
+setupClose.addEventListener('click', function () {
+  setup.classList.add('invisible');
 });

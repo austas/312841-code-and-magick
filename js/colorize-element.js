5948eb4f60a9da325d8f6ac29485575970b2d0fa
Artem Ustas <art4yus@ya.ru>
2017-02-09 13:16:45 +0300

    Добавил обработчик keydown для colorize-element

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -6,4 +6,10 @@ window.colorizeElement = function (element, colors, property) {
   element.addEventListener('click', function () {
     element.style[property] = window.utils.getRandomElementExcept(colors, currentColorOfElement);
   });
+
+  element.addEventListener('keydown', function (evt) {
+    if (window.isActivateEvent(evt)) {
+      element.style[property] = window.utils.getRandomElementExcept(colors, currentColorOfElement);
+    }
+  });
 };

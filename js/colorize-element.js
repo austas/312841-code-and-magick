bdf2e229d15a8dcedcd0a5fe1be6ebb0649407b5
Artem Ustas <art4yus@ya.ru>
2017-02-13 00:34:15 +0300

    Переписал получение текущего (настоящего) цвета через метод getComputedStyle

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,13 +1,16 @@
 'use strict';
 
 window.colorizeElement = function (element, colors, property) {
-  var currentColorOfElement = element.style[property];
 
   element.addEventListener('click', function () {
+    var computedStyle = getComputedStyle(element);
+    var currentColorOfElement = computedStyle[property];
     element.style[property] = window.utils.getRandomElementExcept(colors, currentColorOfElement);
   });
 
   element.addEventListener('keydown', function (evt) {
+    var computedStyle = getComputedStyle(element);
+    var currentColorOfElement = computedStyle[property];
     if (window.isActivateEvent(evt)) {
       element.style[property] = window.utils.getRandomElementExcept(colors, currentColorOfElement);
     }
